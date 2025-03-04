a50b66e98aec4d5654315126f437e80c940f65a9
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2025-03-04 03:43:38 +0200

    add dark mode icons

--- src/components/layouts/TheNavbar.vue
+++ src/components/layouts/TheNavbar.vue
@@ -74,16 +74,38 @@
             >FAQs</RouterLink
           >
         </li>
-        <li>
-          <button class="hover:cursor-pointer" @click="toggleDarkMode">
-            Dark
-          </button>
-        </li>
+        <li></li>
       </ul>
 
       <!-- Auth Buttons (desktop) -->
       <div class="hidden md:flex items-center space-x-4">
         <!-- Show these buttons when user is NOT authenticated -->
+        <button class="hover:cursor-pointer" @click="toggleDarkMode">
+          <svg
+            v-if="!isDarkMode"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="size-6"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <svg
+            v-else
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="size-6"
+          >
+            <path
+              d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z"
+            />
+          </svg>
+        </button>
         <template v-if="!isAuthenticated">
           <BaseButton
             @click="showSigninForm = true"
@@ -259,16 +281,37 @@
                 >FAQs</RouterLink
               >
             </li>
-            <li>
-              <button class="hover:cursor-pointer" @click="toggleDarkMode">
-                Dark
-              </button>
-            </li>
           </ul>
 
           <!-- Mobile Auth Buttons -->
           <div class="flex flex-col space-y-4">
             <!-- Show these buttons when user is NOT authenticated -->
+            <button class="hover:cursor-pointer" @click="toggleDarkMode">
+              <svg
+                v-if="!isDarkMode"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              <svg
+                v-else
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6"
+              >
+                <path
+                  d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z"
+                />
+              </svg>
+            </button>
             <template v-if="!isAuthenticated">
               <BaseButton
                 @click="
@@ -402,6 +445,7 @@ import store from "@/store/store";
 import Signup from "../registration/Signup.vue";
 import Signin from "../registration/Signin.vue";
 import { logoutUser } from "@/firebase";
+import { computed } from "vue";
 
 export default {
   data() {
@@ -418,6 +462,9 @@ export default {
     },
     userEmail() {
       return this.$store.state.user ? this.$store.state.user.email : "";
+    },
+    isDarkMode() {
+      return store.state.isDarkMode;
     },
   },
   components: {
