aadfa419ea5dcd9a41a6843e190e561427113598
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2025-03-05 23:16:44 +0200

    change firebase database

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, onValue } from "firebase/database";
-import { getAuth, onAuthStateChanged, setPersistence, browserLocalPersistence, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  onAuthStateChanged,
+  setPersistence,
+  browserLocalPersistence,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBQkgVpE_7RTWTwiV33hHzhtHeMMEIDp3k",
-  authDomain: "reservio-77386.firebaseapp.com",
-  databaseURL:
-    "https://reservio-77386-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "reservio-77386",
-  storageBucket: "reservio-77386.firebasestorage.app",
-  messagingSenderId: "646436536543",
-  appId: "1:646436536543:web:059a2822cae584d5d7296a",
+  apiKey: "AIzaSyCU6X4lJ_bFanc3x6GExHlRTqfSgdxG8ZI",
+  authDomain: "reservio-f608f.firebaseapp.com",
+  projectId: "reservio-f608f",
+  storageBucket: "reservio-f608f.firebasestorage.app",
+  messagingSenderId: "431688922050",
+  appId: "1:431688922050:web:21daf5a90cf9ba0f925c5b",
 };
 
 // Initialize Firebase
@@ -31,4 +36,11 @@ setPersistence(auth, browserLocalPersistence)
     console.error("Error setting persistence:", error);
   });
 
-export { ref, set, onValue, onAuthStateChanged, GoogleAuthProvider, signInWithPopup };
+export {
+  ref,
+  set,
+  onValue,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+};
